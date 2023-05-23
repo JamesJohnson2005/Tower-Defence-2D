e03fe11cb21ee83a83a43ca50123ea31272cc26e
JellyJamDev <jamejohn2005@gmail.com>
2023-05-23 09:12:04 -0400

    AHAHAHHA

---
+++ Tower Defence/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Stats")]
+    [SerializeField] private int playerSpeed;
+}
