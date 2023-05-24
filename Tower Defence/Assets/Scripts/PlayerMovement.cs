36fb37727152b52257b2a0a37c698828bcb3ff13
JellyJamDev <jamejohn2005@gmail.com>
2023-05-24 14:10:32 -0400

    ahahah

--- Tower Defence/Assets/Scripts/PlayerMovement.cs
+++ Tower Defence/Assets/Scripts/PlayerMovement.cs
@@ -6,4 +6,29 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Stats")]
     [SerializeField] private int playerSpeed;
+
+    private float x, y;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D collider;
+
+    private void Awake()
+    {
+        // Declare Components
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        // Get User Input
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // Move Player
+        rb.velocity = new Vector2(x * playerSpeed, y * playerSpeed);
+    }
 }
