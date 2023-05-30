2d2b2cb33e4e679fdace872a401f997a1cb3cb87
JellyJamDev <jamejohn2005@gmail.com>
2023-05-30 09:45:43 -0400

    GameManager

---
+++ Tower Defence/Assets/Scripts/Menus/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameObject gameManager;
+
+    private void Awake()
+    {
+        // Keep the GameManager between scenes
+        DontDestroyOnLoad(gameObject);
+        
+        // Set a gamemanager if it doesnt already exist
+        if (gameManager == null)
+        {
+            gameManager = gameObject;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
