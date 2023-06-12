da6ca74e8735bf8aba4d4553eaf384548daf8bd1
kianglen950 <kianglen950@ed.amdsb.ca>
2023-06-12 09:26:09 -0400

    Update PlayerMovement.cs

--- Tower Defence/Assets/Scripts/PlayerMovement.cs
+++ Tower Defence/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Stats")]
     public int playerSpeed;
+    public int playerDamage;
+    public float fireSpeed;
 
     private float x, y;
 
     private Rigidbody2D rb;
     private BoxCollider2D collider;
+    private float fireTimer;
+    public GameObject bulletPrefab;
 
     private void Awake()
     {
@@ -21,14 +25,41 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        fireTimer -= Time.deltaTime;
+
         // Get User Input
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
-    }
+        
+        if (Input.GetButton("Fire1"))
+        {
+            PlayerShoot();
+        }
+    }   
 
     private void FixedUpdate()
     {
         // Move Player
         rb.velocity = new Vector2(x * playerSpeed, y * playerSpeed);
+    }
+    private void PlayerShoot()
+    {
+        // Can the tower shoot?
+        if (fireTimer <= 0)
+        {
+            // Spawn a bullet object
+            GameObject spawnedBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            // Make bullet face the same way as the tower
+            spawnedBullet.transform.right = -transform.up;
+
+            spawnedBullet.GetComponent<BulletManager>().SetValues(playerDamage, null);
+
+            // Destroy the bullet after 5 seconds if it somehow manages to stay 'alive' that long
+            Destroy(spawnedBullet, 5);
+
+            // Reset timer
+            fireTimer = fireSpeed;
+        }
     }
 }
