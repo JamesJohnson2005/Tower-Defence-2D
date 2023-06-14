31dd130a5bed5c56a34300cfcba49426cacc6711
JellyJamDev <jamejohn2005@gmail.com>
2023-06-14 08:58:26 -0400

    Tower Purchase

--- Tower Defence/Assets/Scripts/TowerBase.cs
+++ Tower Defence/Assets/Scripts/TowerBase.cs
@@ -4,40 +4,92 @@ using UnityEngine;
 
 public class TowerBase : MonoBehaviour
 {
+    public enum TowerType
+    {
+        Basic,
+        Gold,
+        Red
+    }
+
     private bool canBuy;
-    private bool menuUp;
+    public bool menuUp;
     public GameObject buyMenu;
+    public TowerType currentType;
+    public bool hasTower;
+    public GameObject baseTower, goldTower, redTower;
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !hasTower)
         {
             collision.gameObject.GetComponent<PlayerMovement>().purchaseText.SetActive(true);
             canBuy = true;
+            gameManager.selectedBase = gameObject;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !hasTower)
         {
             collision.gameObject.GetComponent<PlayerMovement>().purchaseText.SetActive(false);
             canBuy = false;
             menuUp = false;
+            gameManager.selectedBase = null;
+            buyMenu.SetActive(false);
         }
     }
 
     public void Update()
     {
-        // Make the menu show
-        buyMenu.SetActive(menuUp);
+        
 
         if (canBuy)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 menuUp = true;
+                buyMenu.SetActive(true);
             }
         }
+    }
+
+    public void PlaceTower(int type)
+    {
+        hasTower = true;
+        canBuy = false;
+        menuUp = false;
+        buyMenu.SetActive(false);
+        switch (type)
+        {
+            case 1:
+                SetTower(baseTower);
+                break;
+            case 2:
+                SetTower(goldTower);
+                break;
+            case 3:
+                SetTower(redTower);
+                break;
+        }
+        gameManager.selectedBase = null;
+
+    }
+
+    private void SetTower(GameObject newType)
+    {
+        // Disable all
+        baseTower.SetActive(false);
+        goldTower.SetActive(false);
+        redTower.SetActive(false);
+
+        // Set the new one as active
+        newType.SetActive(true);
     }
 }
