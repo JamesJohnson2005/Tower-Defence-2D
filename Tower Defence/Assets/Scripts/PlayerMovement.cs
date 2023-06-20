e59c75ab4edbc76130f4a98bfdc532c6f12cf579
JellyJamDev <jamejohn2005@gmail.com>
2023-06-20 09:56:15 -0400

    10x speed button

--- Tower Defence/Assets/Scripts/PlayerMovement.cs
+++ Tower Defence/Assets/Scripts/PlayerMovement.cs
@@ -34,6 +34,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 worldPosFlattened = new Vector3(worldPosition.x, worldPosition.y, 0);
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 10;
+            } else if (Time.timeScale == 10)
+            {
+                Time.timeScale = 1;
+            }
+        }
+
         transform.right = Vector3.Lerp(transform.right, worldPosFlattened - transform.position, Time.deltaTime * 10);
 
         // Get User Input
